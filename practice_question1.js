e187268b9781467275e85af3ef393b40e158c627
nikita-gupta2 <nikita987dsa@gmail.com>
2025-12-25 18:04:35 +0530

    First practice question

---
+++ practice_question1.js
@@ -0,0 +1,14 @@
+// Traffic Light System
+let color = "red";
+
+if(color === "red"){
+    console.log("Stop");
+}
+if(color === "green"){
+    console.log("Go");
+}
+if(color === "yellow"){
+    console.log("Slow down");
+}
+
+
