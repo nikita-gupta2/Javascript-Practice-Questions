dedc18fe3e0cf7e04bec99ad2f534e8469b60ef2
nikita-gupta2 <nikita987dsa@gmail.com>
2025-12-25 18:05:15 +0530

    2nd ques

---
+++ practice_ques_2.js
@@ -0,0 +1,19 @@
+// Popcorn Price
+
+let popcornSize = 'M';
+
+if (popcornSize === 'XL'){
+    console.log("Please pay 250");
+}
+else if(popcornSize === 'L'){
+    console.log("Please pay 200");
+}
+else if(popcornSize === 'M'){
+    console.log("Please pay 100");
+}
+else if(popcornSize === 'S'){
+    console.log("Please pay 50");
+}
+else{
+    console.log("Please Choose valid Size");
+}
