f8a5b4c23940e89429833275a008e8ffb6b42a88
nikita-gupta2 <nikita987dsa@gmail.com>
2025-12-27 19:56:33 +0530

    ques 7

---
+++ practice_ques_7.js
@@ -0,0 +1,19 @@
+
+let quarter = 3;
+
+switch(quarter){
+    case 1:
+        console.log("Jan, feb,march");
+        break;
+    case 2:
+        console.log("Apr, may ,june");
+        break;
+    case 3:
+        console.log("July, Aug,Sept");
+        break;
+    case 4:
+        console.log("oct, nov,dec");
+        break;
+    default:
+        console.log("enter valid quarter");
+}
