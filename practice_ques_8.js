be416a00a685b43f62a9fc2a3a7aa2f2b47b8608
nikita-gupta2 <nikita987dsa@gmail.com>
2025-12-28 19:55:32 +0530

    8th ques

---
+++ practice_ques_8.js
@@ -0,0 +1,9 @@
+
+let string = "abdcer";
+
+if ((string[0] === 'a' || string[0] === 'A') && string.length > 5){
+    console.log("Golden String");
+}
+else{
+    console.log("Not Golden String");
+}
