409b7e3aaecafc8b3b2a4a38a922ccfedf24b056
nikita-gupta2 <nikita987dsa@gmail.com>
2025-12-28 19:56:06 +0530

    9th ques

---
+++ practice_ques_9.js
@@ -0,0 +1,14 @@
+
+let num1 =15;
+let num2 =9;
+let num3 = 8;
+
+if ((num1 > num2) && (num1 > num3)){
+    console.log("num 1 is largest");
+}
+else if (num2 > num3){
+    console.log("num 2 is largest");
+}
+else {
+    console.log("num3 is largest");
+}
