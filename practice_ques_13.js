2209ea62e7570dd1aa5f1a3df14ab5787ff5c86b
nikita-gupta2 <nikita987dsa@gmail.com>
2025-12-30 21:13:47 +0530

    13th ques

---
+++ practice_ques_13.js
@@ -0,0 +1,8 @@
+
+let arr = ['jan','july','march','aug'];
+console.log(arr.shift());
+console.log(arr.shift());
+console.log(arr);
+console.log(arr.unshift('june'));
+console.log(arr.unshift('july'));
+console.log(arr);
