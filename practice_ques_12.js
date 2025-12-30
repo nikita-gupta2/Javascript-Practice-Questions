a1b3a781c686cd0d65b93003b859aad244afe633
nikita-gupta2 <nikita987dsa@gmail.com>
2025-12-30 21:13:23 +0530

    12th ques

---
+++ practice_ques_12.js
@@ -0,0 +1,12 @@
+
+let name = "ApnaCollege";
+
+console.log(name.slice(4,9));
+console.log(name.indexOf("na"));
+console.log(name.replace("Apna","Our"));
+
+console.log(name.slice(4).replace('l','t').replace('l','t'));
+// other method 
+//make a new string with cotlege and then again replace
+
+
