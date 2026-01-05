c8f7ebb1f878cda8ece9c08d5beb89d0d3c03775
nikita-gupta2 <nikita987dsa@gmail.com>
2026-01-05 20:47:39 +0530

    practice_ques_21

---
+++ practice_ques_21.js
@@ -0,0 +1,17 @@
+
+let car = {
+    name : "maruti suzuki",
+    model: "Dzire",
+    color: " white"
+}
+console.log(car);
+let person = {
+    name : "alen",
+    age: 25,
+    city: "NY"
+}
+console.log(person);
+person.city = "New York";
+console.log(person);
+person.country= "US";
+console.log(person);
